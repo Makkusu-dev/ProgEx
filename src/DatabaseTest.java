af06caae909ac6e2d5ed8717d6756a1da204af78
MaxEhs <ehringha@stud.fra-uas.de>
2016-11-22 17:45:32 +0100

    Datenbank via SQLite

---
+++ src/DatabaseTest.java
@@ -0,0 +1,137 @@
+import java.sql.*;
+/*
+ * Establishes a connection to the local Database (database.db), and executes a few SQL queries for testing purposes.
+ */
+public class DatabaseTest {
+
+	public static void main(String[] args) {
+		Connection c = null;
+		Statement stmt = null;
+		try {
+
+			Class.forName("org.sqlite.JDBC");											//Connecting to the database
+			c = DriverManager.getConnection("jdbc:sqlite:database.db");
+			
+		} catch (Exception e) {
+			System.out.println(e);
+			System.exit(0);
+		}
+
+		System.out.println("Database connection established.");
+
+		try {
+			
+			stmt = c.createStatement();													//Creating a basic table named "Patient" with a few parameters
+			String sql = "CREATE TABLE Patient" +
+						 "(ID INT PRIMARY KEY     NOT NULL," +
+						 " NAME           TEXT    NOT NULL)";
+			
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.close();																	//Closing database connection (for testing purposes)
+
+		} catch (Exception e) {
+			System.out.println(e);
+			System.exit(0);
+		}
+		
+		System.out.println("Table created!");
+		
+	    try {
+	    	
+	        c = DriverManager.getConnection("jdbc:sqlite:database.db");					//Reinitializing database connection
+	        c.setAutoCommit(false);														//AutoCommit=FALSE (manual commit needed)
+
+	        stmt = c.createStatement();													//Defining a statement
+	        
+	        String sql = "INSERT INTO Patient (ID,NAME) " +								//SQL insert statement for populating the new table with some data
+	        			 "VALUES (1, 'Der Weihnachtsmann');"; 
+	        stmt.executeUpdate(sql);													//Executing statement
+       
+       		sql = "INSERT INTO Patient (ID,NAME) " +
+       			  "VALUES (2, 'Bear Grylls');";
+       		stmt.executeUpdate(sql);													//Executing statement
+       		
+            stmt.close();
+            c.commit();																	//Committing
+            
+          } catch (Exception e) {
+  			System.out.println(e);
+  			System.exit(0);
+  		}
+	        
+	    System.out.println("Data inserted!");
+	    
+	    try {
+	    	
+	        stmt = c.createStatement();													//SELECT statement with a loop for listing all table entries
+	        ResultSet rs = stmt.executeQuery( "SELECT * FROM Patient;" );
+	        while ( rs.next() ) {
+	           int id = rs.getInt("id");
+	           String  name = rs.getString("name");
+	           System.out.println( "ID = " + id );
+	           System.out.println( "NAME = " + name );
+	           System.out.println();
+	        }
+	        
+	        rs.close();
+	        stmt.close();
+	        c.close();																	//Closing database connection
+	           
+          } catch (Exception e) {
+  			System.out.println(e);
+  			System.exit(0);
+  		}
+	    
+	    try {
+	    	
+	        c = DriverManager.getConnection("jdbc:sqlite:database.db");					//Reinitializing database connection
+	        c.setAutoCommit(false);
+
+	        stmt = c.createStatement();
+	        String sql = "UPDATE Patient set name = 'Tot' where ID=1;";					//Updating a data set
+	        stmt.executeUpdate(sql);
+	        
+	        sql = "DELETE FROM Patient where ID=2;";									//Deleting a data set
+	        stmt.executeUpdate(sql);
+	        
+	        c.commit();
+	        
+	        ResultSet rs = stmt.executeQuery( "SELECT * FROM Patient;" );				//Another SELECT statement for listing all table entries
+	        while ( rs.next() ) {
+	           int id = rs.getInt("id");
+	           String  name = rs.getString("name");
+	           System.out.println( "ID = " + id );
+	           System.out.println( "NAME = " + name );
+	           System.out.println();
+	        }
+	        
+	        rs.close();
+	        stmt.close();
+	        c.close();																	//Closing database connection (weil's so viel Spaﬂ macht)
+	           
+          } catch (Exception e) {
+  			System.out.println(e);
+  			System.exit(0);
+  		}
+	    
+	    try {
+	    	
+	        c = DriverManager.getConnection("jdbc:sqlite:database.db");					//Reinitializing database connection
+	        c.setAutoCommit(false);
+	        
+	        stmt = c.createStatement();
+	        String sql = "DROP TABLE Patient;";											//Dropping the table "Patient"
+	        stmt.executeUpdate(sql);
+	        
+	        c.commit();
+	        c.close();																	//Closing database connection
+	        
+	    } catch (Exception e) {
+	    	System.out.println(e);
+	    	System.exit(0);
+	    }
+		
+	}
+
+}
